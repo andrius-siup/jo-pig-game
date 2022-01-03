da0438a1208b4c028f5da34280dc0377840ed171
andrius-siup <andrius.siupinys@gmail.com>
2022-01-03 17:34:07 +0000

    Displayed player 1 score by added sum of random dice

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,11 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+// Player 1 current score
+const current0El = document.getElementById('current--0');
+// Player 2 current score
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,6 +18,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
@@ -21,5 +28,14 @@ btnRoll.addEventListener('click', function () {
   // 2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `images/dice-${dice}.png`;
+
   // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // player1 score, change later
+  } else {
+    // switch to next player
+    
+  }
 });
